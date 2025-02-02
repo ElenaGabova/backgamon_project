d97fa3662525d36fee0d093270a10622882ecf8e
ElenaGabova <eleyurgab@gmail.com>
2025-02-02 21:36:41 +0300

    Добавьте файлы проекта.

---
+++ backgamon_project/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var numbers = ReadData();
+        int top3sum = numbers.OrderByDescending(x => x).Take(3).Sum();
+        Console.WriteLine($"\n\nСумма очков 3 лучших команд:{top3sum}");
+    }
+
+    /// <summary>
+    /// Метод считывает данные с консоли, проверяет корректность ввода
+    /// </summary>
+    /// <returns></returns>
+    private static List<int> ReadData()
+    {
+        List<int> numbers = new List<int>();
+        string input = string.Empty;
+        int number = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            number = 0;
+            while (true)
+            {
+                Console.WriteLine($"Введите значение очков для {i + 1} команды:");
+                input = Console.ReadLine();
+                if (!int.TryParse(input, out number) || number <= 0)
+                    Console.WriteLine("Некорректный ввод, значение должно быть числом больше 0.");
+                else
+                {
+                    numbers.Add(number);
+                    break;
+                }
+            }
+        }
+        return numbers;
+    }
+}
